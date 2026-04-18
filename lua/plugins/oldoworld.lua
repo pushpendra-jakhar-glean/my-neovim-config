13794442882f3503e6811fb63b7bd3d5f6b71a7e
Pushpendra Jakhar <pushpendra.jakhar@Pushpendras-MacBook-Pro.local>
2026-04-18 07:00:41 +0530

    Override red highlight groups with warm forest green palette

--- lua/plugins/oldoworld.lua
+++ lua/plugins/oldoworld.lua
@@ -5,6 +5,15 @@ return {
     config = function()
       require("oldworld").setup({
         variant = "oled",
+        highlight_overrides = {
+          ["@namespace"] = { fg = "#88c4a0", italic = true },
+          ["@lsp.type.namespace"] = { fg = "#88c4a0", italic = true },
+          ["@keyword.import"] = { fg = "#7aad96" },
+          Structure = { fg = "#a3b585" },
+          ["@lsp.type.class"] = { fg = "#a3b585" },
+          ["@type"] = { fg = "#a3b585" },
+          ["@constructor"] = { fg = "#a3b585" },
+        },
       })
       vim.cmd.colorscheme("oldworld")
     end,
